35503cf6c8e0be30b1b75f2a76085b20c6a2ed5d
Vianney-Gln <vgeloen@gmail.com>
2022-07-29 17:24:11 +0200

    end integration static

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 // Components imports
 import Header from '../Header/Header';
 import MenuLeft from '../Menu-left/MenuLeft';
+import MenuTop from '../Menu-top/MenuTop';
 
 // style css
 import './app.scss';
@@ -9,6 +10,7 @@ const App = () => {
     <div className="App">
       <Header />
       <MenuLeft />
+      <MenuTop />
     </div>
   );
 };
