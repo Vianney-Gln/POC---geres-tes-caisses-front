3377adb90ac8c3c40cabfb929cb2ed56e23c352e
Vianney-Gln <vgeloen@gmail.com>
2022-07-29 15:47:23 +0200

    add static select / option in component MenuLeft

--- src/components/Menu-left/MenuLeft.jsx
+++ src/components/Menu-left/MenuLeft.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 import './menuLeft.scss';
 
 const MenuLeft = () => {
-  return <div className="menu-left">MenuLeft</div>;
+  return (
+    <div className="menu-left">
+      <select>
+        <option>Caisses vracs</option>
+        <option>Caisses total</option>
+        <option>Fagots</option>
+      </select>
+    </div>
+  );
 };
 
 export default MenuLeft;
