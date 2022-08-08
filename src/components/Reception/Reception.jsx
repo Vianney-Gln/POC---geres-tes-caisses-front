2cc3a9b3c5326d7bb9aa487c0ac2a18429e4e942
Vianney-Gln <vgeloen@gmail.com>
2022-08-08 22:11:52 +0200

    init a form into the reception component

--- src/components/Reception/Reception.jsx
+++ src/components/Reception/Reception.jsx
@@ -12,7 +12,35 @@ const Reception = () => {
   useEffect(() => {
     setActivate(false);
   }, []);
-  return <div className="container-reception">Reception</div>;
+  return (
+    <div className="container-reception">
+      <h2>Reception</h2>
+      <form className="form-reception">
+        <div className="container-form">
+          <label htmlFor="idBoxe">
+            <span>Identifiant caisse:</span>
+            <input type="text" name="idBoxe"></input>
+          </label>
+          <label htmlFor="articleBoxe">
+            <span>type de caisse:</span>
+            <select>
+              <option>Value 1</option>
+            </select>
+          </label>
+          <label htmlFor="button-next-article">
+            <button name="button-next-article" type="button">
+              Article suivant
+            </button>
+          </label>
+        </div>
+        <div className="container-button-submit">
+          <button className="button-submit" type="submit">
+            Valider la réception
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Reception;
