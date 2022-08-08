7fdf7db93a16545b00dadfb29325785391e60e4f
Vianney-Gln <vgeloen@gmail.com>
2022-08-08 22:41:44 +0200

    test dynamics fields form

--- src/components/Reception/Reception.jsx
+++ src/components/Reception/Reception.jsx
@@ -1,12 +1,16 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 // import context
 import ContextArticles from '../../context/ContextArticles';
 // import style css
 import './reception.scss';
 
 const Reception = () => {
+  // Context
   const contextArticles = useContext(ContextArticles);
   const { setActivate } = contextArticles;
+
+  // States
+  const [line, setLine] = useState(['']);
 
   // function desactivate the Menu-Left on component mounting
   useEffect(() => {
@@ -17,21 +21,30 @@ const Reception = () => {
       <h2>Reception</h2>
       <form className="form-reception">
         <div className="container-form">
-          <label htmlFor="idBoxe">
-            <span>Identifiant caisse:</span>
-            <input type="text" name="idBoxe"></input>
-          </label>
-          <label htmlFor="articleBoxe">
-            <span>type de caisse:</span>
-            <select>
-              <option>Value 1</option>
-            </select>
-          </label>
-          <label htmlFor="button-next-article">
-            <button name="button-next-article" type="button">
-              Article suivant
-            </button>
-          </label>
+          {line.map((elt, index) => {
+            return (
+              <div key={index} className="newLine">
+                <label htmlFor="idBoxe">
+                  <span>Identifiant caisse:</span>
+                  <input type="text" name="idBoxe"></input>
+                </label>
+                <label htmlFor="articleBoxe">
+                  <span>type de caisse:</span>
+                  <select>
+                    <option>Value 1</option>
+                  </select>
+                </label>
+                <label htmlFor="button-next-article">
+                  <button
+                    onClick={() => setLine((old) => [...old, elt])}
+                    name="button-next-article"
+                    type="button">
+                    Article suivant
+                  </button>
+                </label>
+              </div>
+            );
+          })}
         </div>
         <div className="container-button-submit">
           <button className="button-submit" type="submit">
