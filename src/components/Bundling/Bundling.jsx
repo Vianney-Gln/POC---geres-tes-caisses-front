0153494c747377bad8cd9e5b12206ad1b2abbbba
Vianney-Gln <vgeloen@gmail.com>
2022-08-18 17:26:41 +0200

    MenuLeft disabled in component Bundling

--- src/components/Bundling/Bundling.jsx
+++ src/components/Bundling/Bundling.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 // import style css
 import './bundling.scss';
-
+// import Context
+import ContextArticles from '../../context/ContextArticles';
 const Bundling = () => {
+  const contextArticle = useContext(ContextArticles);
+  const { setActivate } = contextArticle;
+
+  useEffect(() => {
+    setActivate(false);
+  }, []);
+
   return <div className="container-bundling">Bundling</div>;
 };
 
