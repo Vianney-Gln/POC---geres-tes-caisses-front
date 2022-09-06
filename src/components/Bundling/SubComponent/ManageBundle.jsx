cdfb5b0a50e84aabc4da142ca8585f0693458114
Vianney-Gln <vgeloen@gmail.com>
2022-09-06 16:37:36 +0200

    re use component cardsStock in manageBundle component

--- src/components/Bundling/SubComponent/ManageBundle.jsx
+++ src/components/Bundling/SubComponent/ManageBundle.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+// Import style css
+import './manageBundle.scss';
+// Import services
+import { getFagots } from '../../../services/stock';
+// Import components
+import CardsStockFagot from '../../cardsStockFagots/CardsStockFagot';
 const ManageBundle = () => {
-  return <div>ManageBundle</div>;
+  // States
+  const [fagots, setFagots] = useState([]);
+  useEffect(() => {
+    getFagots().then((result) => setFagots(result.data));
+  }, []);
+
+  return (
+    <div className="container-manageBundle">
+      <ul className="list-cards">
+        {fagots.length ? (
+          fagots.map((elt, index) => (
+            <CardsStockFagot key={index} stock={elt} fagotId={elt.fagotId} />
+          ))
+        ) : (
+          <p className="no-fagot-found">Aucun fagot trouvé</p>
+        )}
+      </ul>
+    </div>
+  );
 };
 
 export default ManageBundle;
--- src/components/cardsStockFagots/CardsStockFagot.jsx
+++ src/components/cardsStockFagots/CardsStockFagot.jsx
@@ -1,7 +1,7 @@
 // import react hooks
 import React, { useEffect, useState } from 'react';
-//import Routing Link
-import { Link } from 'react-router-dom';
+// import Routing Link
+import { Link, useParams } from 'react-router-dom';
 // import PropTypes
 import PropTypes from 'prop-types';
 // import style css
@@ -10,18 +10,34 @@ import './cardsStockFagot.scss';
 import { getCountBoxesByFagot } from '../../services/stock';
 
 const CardsStockFagot = ({ stock, fagotId }) => {
-  const [nbBoxe, setNbBoxe] = useState(null);
+  const [nbBoxe, setNbBoxe] = useState(null); // nb boxes
+  const param = useParams();
+  const { operation } = param;
+
   useEffect(() => {
     getCountBoxesByFagot(fagotId).then((result) => setNbBoxe(result.nbBoxes));
   }, []);
   return (
-    <Link to={`/fagot-content/${fagotId}`}>
-      <li className="card-fagot">
-        <span>{stock.uuid}</span>
-        <span>{stock.fagotType}</span>
-        <span className="nbr-boxes">Nombre de caisses: {nbBoxe}/10</span>
-      </li>
-    </Link>
+    <>
+      {operation === 'manage-bundle' ? (
+        <div className="card">
+          <li className="card-fagot">
+            <span>{stock.uuid}</span>
+            <span>{stock.fagotType}</span>
+            <span className="nbr-boxes">Nombre de caisses: {nbBoxe}/10</span>
+            <button type="button">Défagoter</button>
+          </li>
+        </div>
+      ) : (
+        <Link to={`/fagot-content/${fagotId}`}>
+          <li className="card-fagot">
+            <span>{stock.uuid}</span>
+            <span>{stock.fagotType}</span>
+            <span className="nbr-boxes">Nombre de caisses: {nbBoxe}/10</span>
+          </li>
+        </Link>
+      )}
+    </>
   );
 };
 
