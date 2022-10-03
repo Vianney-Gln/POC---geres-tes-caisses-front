d5ce6c9535f07493865c4e77774b9aa35aaf312e
Vianney-Gln <vgeloen@gmail.com>
2022-10-03 21:18:07 +0200

    chore: Bundling cleaned

--- src/components/Bundling/Bundling.jsx
+++ src/components/Bundling/Bundling.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-// import reac-router-dom
 import { useParams } from 'react-router-dom';
-// import style css
 import './bundling.scss';
-// import Components
 import CreateBundle from './SubComponent/CreateBundle';
 import ManageBundle from '../Bundling/SubComponent/ManageBundle';
 import Bundle from './SubComponent/Bundle';
 const Bundling = () => {
-  // useParams
   const param = useParams();
   const { operation } = param;
 
