4548b5b67ab562889c6e8ca8e6d53269e39924e5
Vianney-Gln <vgeloen@gmail.com>
2022-10-03 21:20:23 +0200

    chore: Bundle cleaned

--- src/components/Bundling/SubComponent/Bundle.jsx
+++ src/components/Bundling/SubComponent/Bundle.jsx
@@ -1,25 +1,19 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
-// Import style css
 import './bundle.scss';
-// Import components
 import ContentBundle from '../../contentBundle/ContentBundle';
 import Stock from '../../stock/Stock';
-// import PropTypes
 import PropTypes from 'prop-types';
-// Import component context
 import ContextBundles from '../../../context/ContextBundles';
 
 const Bundle = ({ operation }) => {
-  // Context
   const contextBundles = useContext(ContextBundles);
 
-  // Params
   const param = useParams();
 
+  // On component mounting set the current id bundle equal to the param id
   useEffect(() => {
     contextBundles.setCurrentIdBundle(param.id);
   }, []);
