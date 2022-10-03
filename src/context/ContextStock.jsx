1d00158345f0e89ce64d0463896ba2786409a828
Vianney-Gln <vgeloen@gmail.com>
2022-10-03 18:15:35 +0200

    chore: ContextStock cleaned

--- src/context/ContextStock.jsx
+++ src/context/ContextStock.jsx
@@ -1,19 +1,14 @@
-// imports hooks
 import React, { createContext, useState, useEffect, useContext } from 'react';
-// import proptypes
 import PropTypes from 'prop-types';
-// import services
 import { getCountTotal, getCountBundle, getCountVrac } from '../services/stock.js';
-// Import component context
 import ContextBundles from './ContextBundles.jsx';
-// create context
+
 const ContextStock = createContext(null);
 
 export const ContextStockProvider = ({ children }) => {
-  // States
-  const [typeStock, setTypeStock] = useState('caisses-vrac'); // state getting the type of stock (vrac, total, fagots) --- default caisses-vrac
-  const [numberBoxes, setNumberBoxes] = useState(null); // state getting the bumber of boxes
-  const [idArticleForCountFunctions, setIdArticleCountForCountFunctions] = useState(null); //state id article argument getCount() functions
+  const [typeStock, setTypeStock] = useState('caisses-vrac');
+  const [numberBoxes, setNumberBoxes] = useState(null);
+  const [idArticleForCountFunctions, setIdArticleCountForCountFunctions] = useState(null);
 
   // Get other Context -- Use restart Effect from ContextBundles to restart API calls
   const { restartEffect } = useContext(ContextBundles);
