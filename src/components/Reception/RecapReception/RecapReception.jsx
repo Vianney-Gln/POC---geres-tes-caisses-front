6ea19aefd1160b9f7596d22a3758c41305fa4da5
Vianney-Gln <vgeloen@gmail.com>
2022-10-16 15:16:15 +0200

    feat: init RecapReception component

---
+++ src/components/Reception/RecapReception/RecapReception.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './recapReception.scss';
+
+const RecapReception = () => {
+  return (
+    <div className="container-recap-reception">
+      <h2>Recap - Reception</h2>
+    </div>
+  );
+};
+
+export default RecapReception;
